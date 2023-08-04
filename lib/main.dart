d2ccab271caf03d6ed2d85f672b567e28b315acf
shammaskhann <shammas.csgo0312@gmail.com>
2023-08-04 18:15:41 +0500

    no changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,13 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
